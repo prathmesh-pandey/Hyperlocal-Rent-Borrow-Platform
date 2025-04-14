6206c2dedbad35428fc5339376f1709bc179453d
prathmesh-pandey <pandeyprathmesh285@gmail.com>
2025-04-14 17:10:38 +0530

    Initial commit for Hyperlocal Rent-Borrow Platform 🚀

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './App.css';  // Link to our future CSS
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Your Startup</h1>
+        <p>Your amazing app starts here!</p>
+        <button className="start-button">Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
